b16722a545674bc3eab16082f97173e42532c3a4
Parth Makadiya <makadiyaparth98@gmail.com>
2022-02-10 23:13:56 +0530

    add modules and basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'stocks',
+    loadChildren: () =>
+      import('./modules/stock/stock.module').then((m) => m.StockModule),
+  },
+  {
+    path: 'options',
+    loadChildren: () =>
+      import('./modules/options/options.module').then((m) => m.OptionsModule),
+  },
+  {
+    path: 'futures',
+    loadChildren: () =>
+      import('./modules/future/future.module').then((m) => m.FutureModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
