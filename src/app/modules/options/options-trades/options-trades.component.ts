21894d1d05ed96b3ee3a74b1bc69651d4f421b4b
Parth Makadiya <makadiyaparth98@gmail.com>
2022-02-10 23:59:52 +0530

    add dummy options trades and options form

---
+++ src/app/modules/options/options-trades/options-trades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-options-trades',
+  templateUrl: './options-trades.component.html',
+  styleUrls: ['./options-trades.component.scss']
+})
+export class OptionsTradesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/options/options.module.ts
+++ src/app/modules/options/options.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { OptionsRoutingModule } from './options-routing.module';
 import { OptionsComponent } from './options.component';
+import { OptionsFormComponent } from './options-form/options-form.component';
+import { OptionsTradesComponent } from './options-trades/options-trades.component';
 
 
 @NgModule({
   declarations: [
-    OptionsComponent
+    OptionsComponent,
+    OptionsFormComponent,
+    OptionsTradesComponent
   ],
   imports: [
     CommonModule,
