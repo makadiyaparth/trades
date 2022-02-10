1c23edf0915a7a33a32ed473a269b73e5cdec13d
Parth Makadiya <makadiyaparth98@gmail.com>
2022-02-10 23:20:43 +0530

    share things from shared module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+const exports = [HeaderComponent];
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule],
+  declarations: [...exports],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  exports: [...exports],
 })
 export class SharedModule {}
