2f04ea8a85aaf78a37de7f597cd3cfa2aeafc27e
Parth Makadiya <makadiyaparth98@gmail.com>
2022-02-16 18:50:15 +0530

    create components for stock module

---
+++ src/app/modules/stock/components/stock-trades/stock-trades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-trades',
+  templateUrl: './stock-trades.component.html',
+  styleUrls: ['./stock-trades.component.scss']
+})
+export class StockTradesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/stock/stock.module.ts
+++ src/app/modules/stock/stock.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
+import { StockFormComponent } from './components/stock-form/stock-form.component';
+import { StockTradesComponent } from './components/stock-trades/stock-trades.component';
 
 
 @NgModule({
   declarations: [
-    StockComponent
+    StockComponent,
+    StockFormComponent,
+    StockTradesComponent
   ],
   imports: [
     CommonModule,
