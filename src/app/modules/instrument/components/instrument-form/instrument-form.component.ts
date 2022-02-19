87992ceb67e4d9a7b3f89f8d6de1398cb6875c2f
Parth Makadiya <parth.makadiya@neptune-ubi.com>
2022-02-19 16:37:23 +0530

    add instrument components

---
+++ src/app/modules/instrument/components/instrument-form/instrument-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instrument-form',
+  templateUrl: './instrument-form.component.html',
+  styleUrls: ['./instrument-form.component.scss']
+})
+export class InstrumentFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/instrument/instrument.module.ts
+++ src/app/modules/instrument/instrument.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { InstrumentRoutingModule } from './instrument-routing.module';
 import { InstrumentComponent } from './instrument.component';
+import { InstrumentsComponent } from './components/instruments/instruments.component';
+import { InstrumentFormComponent } from './components/instrument-form/instrument-form.component';
 
 @NgModule({
-  declarations: [InstrumentComponent],
+  declarations: [InstrumentComponent, InstrumentsComponent, InstrumentFormComponent],
   imports: [CommonModule, InstrumentRoutingModule],
 })
 export class InstrumentModule {}
