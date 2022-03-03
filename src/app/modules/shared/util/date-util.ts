a65850022b91c4ac7f391c99f909da4426924614
parth makadiya <makadiyaparth98@gmail.com>
2022-03-04 00:26:40 +0530

    fix function to get next thursday

--- src/app/modules/shared/util/date-util.ts
+++ src/app/modules/shared/util/date-util.ts
@@ -1,6 +1,6 @@
-export function getNextThursday() {
+export function getNextThursday(): string {
   var today = new Date();
-  today.setDate(today.getDate() + (3 - today.getDay() + 7) % 7 + 1);
+  today.setDate(today.getDate() + (((5 - today.getDay()) % 7) || 7));
   return today.toISOString().split('T')[0];
 }
 
