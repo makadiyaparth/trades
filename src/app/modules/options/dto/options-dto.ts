26e8a8e97b0f3fafc7de9369a5a6f2a08c0e8096
parth makadiya <makadiyaparth98@gmail.com>
2022-03-04 00:27:02 +0530

    fix options dto

--- src/app/modules/options/dto/options-dto.ts
+++ src/app/modules/options/dto/options-dto.ts
@@ -1,9 +1,10 @@
+import { InstrumentDTO } from "../../instrument/dto/instrument-dto";
 import { TransactionType } from "../../shared/enum/transaction-type.enum";
 import { OptionsType } from "../enum/option-type.enum";
 
 export interface OptionsDTO {
   id: number;
-  instrumentName: string;
+  instrumentDTO: InstrumentDTO;
   strikePrice: number;
   optionsType: OptionsType;
   transactionType: TransactionType;
