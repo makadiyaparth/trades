9af12ff744e5fda2b7e0cd8299111b0ea5c6c7bd
parth makadiya <makadiyaparth98@gmail.com>
2022-03-03 21:37:35 +0530

    import shared module in options module

--- src/app/modules/options/options.module.ts
+++ src/app/modules/options/options.module.ts
@@ -6,6 +6,7 @@ import { OptionsComponent } from './options.component';
 import { OptionsFormComponent } from './components/options-form/options-form.component';
 import { OptionsTradesComponent } from './components/options-trades/options-trades.component';
 import { InstrumentService } from '../instrument/service/instrument.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { InstrumentService } from '../instrument/service/instrument.service';
     OptionsFormComponent,
     OptionsTradesComponent,
   ],
-  imports: [CommonModule, OptionsRoutingModule],
+  imports: [CommonModule, OptionsRoutingModule, SharedModule],
   providers: [InstrumentService]
 })
 export class OptionsModule { }
