5c368a42a1eb870f8b069042b4ccd5470c14a14a
parth makadiya <makadiyaparth98@gmail.com>
2022-03-04 00:28:32 +0530

    create and view options

--- src/app/modules/options/components/options-trades/options-trades.component.ts
+++ src/app/modules/options/components/options-trades/options-trades.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BehaviorSubject, Observable, switchMap } from 'rxjs';
+import { getDateAndMonth } from 'src/app/modules/shared/util/date-util';
+import { OptionsDTO } from '../../dto/options-dto';
+import { OptionsType } from '../../enum/option-type.enum';
+import { OptionsService } from '../../service/options.service';
 
 @Component({
   selector: 'app-options-trades',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./options-trades.component.scss']
 })
 export class OptionsTradesComponent implements OnInit {
+  @Input() refreshList$ = new BehaviorSubject<boolean>(true);
+  @Output() onEdit = new EventEmitter<OptionsDTO>();
 
-  constructor() { }
+  optionsDTOs$: Observable<OptionsDTO[]> = new Observable();
+  optionsDTOs: OptionsDTO[] = [];
+  optionsMap: Map<string, OptionsDTO[]> = new Map();
+
+  constructor(private _service: OptionsService) { }
 
   ngOnInit(): void {
+    this.refreshList$.pipe(switchMap(_ => this._service.findAll())).subscribe(dtos => {
+      this.optionsDTOs = dtos;
+      this.populateMap();
+    });
   }
 
+  edit(optionsDTO: OptionsDTO): void {
+    this.onEdit.emit(optionsDTO);
+  }
+
+  refreshList(): void {
+    this.refreshList$.next(true);
+  }
+
+  private populateMap(): void {
+    this.optionsMap.clear();
+    this.optionsDTOs.forEach(dto => {
+      const key = this.generateKey(dto);
+      if (this.optionsMap.has(key)) {
+        const dtos = this.optionsMap.get(key) ?? [];
+        this.optionsMap.set(key, [...dtos, dto]);
+      } else {
+        this.optionsMap.set(key, [dto]);
+      }
+    });
+  }
+
+  private generateKey(optionsDTO: OptionsDTO): string {
+    const { instrumentDTO, expiryDate, strikePrice, optionsType } = optionsDTO;
+    return `${instrumentDTO.name} ${getDateAndMonth(expiryDate)} ${strikePrice} ${this.getShortForm(optionsType)}`;
+  }
+
+  private getShortForm(optionsType: OptionsType): string {
+    return optionsType === OptionsType.CALL ? 'CE' : 'PE';
+  }
 }
--- src/app/modules/options/options.component.ts
+++ src/app/modules/options/options.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { getNextThursday } from '../shared/util/date-util';
+import { OptionsDTO } from './dto/options-dto';
+import { OptionsType } from './enum/option-type.enum';
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
   styleUrls: ['./options.component.scss']
 })
-export class OptionsComponent implements OnInit {
+export class OptionsComponent {
+  refreshList$ = new BehaviorSubject<boolean>(true);
+  optionsDTO: OptionsDTO = { id: 0, optionsType: OptionsType.CALL, lots: 1, expiryDate: getNextThursday() } as OptionsDTO;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  edit(optionsDTO: OptionsDTO): void {
+    this.optionsDTO = optionsDTO;
   }
 
+  refreshList(): void {
+    this.refreshList$.next(true);
+  }
 }
