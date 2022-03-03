24064ba0ecd41fe9fc94ecfab2a191762ce21a98
parth makadiya <makadiyaparth98@gmail.com>
2022-03-04 00:26:14 +0530

    add date util to get date and month

--- src/app/modules/shared/util/date-util.ts
+++ src/app/modules/shared/util/date-util.ts
@@ -3,3 +3,10 @@ export function getNextThursday() {
   today.setDate(today.getDate() + (3 - today.getDay() + 7) % 7 + 1);
   return today.toISOString().split('T')[0];
 }
+
+export function getDateAndMonth(dateStr: string): string {
+  const date = new Date(dateStr);
+  const monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let month = monthList[date.getMonth()];
+  return `${date.getDate()} ${month}`;
+}
