fa6c404f9f785c539a4b910c14c9f6ae5cb52c99
parth makadiya <makadiyaparth98@gmail.com>
2022-03-06 12:04:57 +0530

    add focus directive

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FocusDirective } from './directive/focus.directive';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FocusDirective],
   imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterModule, HttpClientModule],
-  exports: [HeaderComponent, ReactiveFormsModule, FormsModule, RouterModule, HttpClientModule],
+  exports: [HeaderComponent, FocusDirective, ReactiveFormsModule, FormsModule, RouterModule, HttpClientModule],
 })
 export class SharedModule { }
